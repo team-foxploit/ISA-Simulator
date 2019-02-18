027c224c98df39d7736c3b54ba35c2f0f5c1480e
luk3Sky <ganindudananja@gmail.com>
2019-02-18 10:05:45 +0530

    binaryToDecimal added

--- src/com/foxploit/InstructionMemory.java
+++ src/com/foxploit/InstructionMemory.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class InstructionMemory extends Memory{
 
     // Program counter
-    public static int p_c = 0;
+    public int p_c = 0;
 
     //Create a instruction memory
     private Map<Integer, Integer> instructionMemory = new HashMap<>();
@@ -19,5 +19,21 @@ public class InstructionMemory extends Memory{
     @Override
     protected void setData(int address, int data) {
         instructionMemory.put(address, data);
+        p_c ++;
+    }
+
+    // Convert binary to decimal
+    static int binaryToDecimal(int n){
+        int num = n;
+        int dec_value = 0;
+        int base = 1;
+        int temp = num;
+        while (temp > 0){
+            int last_digit = temp % 10;
+            temp = temp / 10;
+            dec_value += last_digit * base;
+            base = base * 2;
+        }
+        return dec_value;
     }
 }
--- src/com/foxploit/Main.java
+++ src/com/foxploit/Main.java
@@ -9,13 +9,36 @@ public class Main {
             "11110000111010001000010100000110"
     };
 
+    static int binaryToDecimal(int n){
+        int num = n;
+        int dec_value = 0;
+
+        // Initializing base
+        // value to 1, i.e 2^0
+        int base = 1;
+
+        int temp = num;
+        while (temp > 0){
+            int last_digit = temp % 10;
+            temp = temp / 10;
+
+            dec_value += last_digit * base;
+
+            base = base * 2;
+        }
+
+        return dec_value;
+    }
+
     public static void main(String[] args) {
 
         // There should be only one register file
-        System.out.println("Hello");
         RegisterFile regFile = new RegisterFile();
         regFile.setData(31, 10);
-        System.out.println(regFile.getData(30));
+        System.out.println(regFile.getData(31));
+
+        // test binaryToDecimal method
+        // System.out.println(binaryToDecimal(Integer.parseInt(new Main().smpl_instr[1].substring(0, 5))));
 
         // Create a InstructionMemory instance
         Memory instMem = new InstructionMemory();
