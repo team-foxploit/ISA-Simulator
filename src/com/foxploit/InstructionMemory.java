fb8e4cff36f5b57082b53a0d1d7b65445fe95bce
luk3Sky <ganindudananja@gmail.com>
2019-02-18 15:23:59 +0530

    Revert "Memory rearranged"
    This reverts commit 11b8a98d2faa29d0e252d924869af02c197f998b.

--- src/com/foxploit/InstructionMemory.java
+++ src/com/foxploit/InstructionMemory.java
@@ -7,53 +7,20 @@ public class InstructionMemory extends Memory{
     public int p_c = 0;
 
     //Create a instruction memory
-    private Map<Integer, String> instructionMemory = new HashMap<>();
+    private Map<Integer, Integer> instructionMemory = new HashMap<>();
 
     // Get data from instruction memory
     @Override
-    public String getData(int address) {
+    public int getData(int address) {
         return instructionMemory.get(address);
-    }
-
-    // Get the instruction memory size
-    public int getSize(){
-        return instructionMemory.size();
     }
 
     // Set data to instruction memory
     @Override
-    protected void setData(int address, String data) {
+    protected void setData(int address, int data) {
         instructionMemory.put(address, data);
-        // System.out.println("set data");
-    }
-
-    @Override
-    protected void setData(int addr, int data) {
-    }
-
-    // Instruction Decode
-    public static int[] instructionDecode(InstructionMemory instMem, int instrAddress){
+        p_c ++;
         // TODO: divide opcode, func and select
-        int[] decoded = new int[3];
-        try{
-            decoded[0] = (int) Integer.parseInt(instMem.getData(instrAddress).substring(0, 6));
-            if(decoded[0] != 0){
-                // System.out.println("non R type");
-            }else{
-                System.out.println("R type");
-                // TODO : set select and funct
-                // TODO : decoded[1], decoded[2]
-            }
-        }catch (NumberFormatException e){
-            System.out.println("Instruction decode failed");
-            System.exit(-1);
-            e.printStackTrace();
-        }
-        return decoded;
-        // Return decoded codes, (opcode, select, funct)
-        // decoded[0] = opcode
-        // decoded[1] = select
-        // decoded[2] = funct
     }
 
     // Convert binary to decimal
--- src/com/foxploit/Memory.java
+++ src/com/foxploit/Memory.java
@@ -2,9 +2,7 @@ package com.foxploit;
 
 public abstract class Memory {
 
-    protected abstract String getData(int addr);
-
-    protected abstract void setData(int addr, String data);
+    protected abstract int getData(int addr);
 
     protected abstract void setData(int addr, int data);
 }
