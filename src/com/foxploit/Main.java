ddaeca8ed250d85a3c652d08e15d78ff5dc4ad51
luk3Sky <ganindudananja@gmail.com>
2019-02-18 22:12:04 +0530

    Input file read cone

--- src/com/foxploit/Main.java
+++ src/com/foxploit/Main.java
@@ -1,47 +1,4 @@
 /*
-package com.foxploit;
-
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-
-
-public class Main {
-
-   /* static String[] smpl_instr = {
-            "00100000000000000000000000001100",
-            "00100000001000000000000000001010",
-            "00000000000000010001000100100000",
-            "11110000111010001000010100000110",
-            "11110000111010001000010100000110",
-            "11110000111010001000010100000110",
-            "11110000111010001000010100000110",
-            "11110000111010001000010100000110",
-            "11111111111111111111111111111111"
-    };
-*/
-/*
-    public static void main(String[] args) {
-
-
-        // Create a Register file instance
-        RegisterFile regFile = new RegisterFile();
-
-        // TODO : cretae real load save instruction
-        // Dummy register load
-        regFile.setData(0, 10);
-        regFile.setData(1, 20);
-        System.out.println(regFile.getData(0));
-        System.out.println(regFile.getData(1));
-
-        // test binaryToDecimal method
-        // System.out.println(binaryToDecimal(Integer.parseInt(new Main().smpl_instr[1].substring(0, 5))));
-
-        // Create a InstructionMemory instance
-        InstructionMemory instMem = new InstructionMemory();
-
-
         //read the input file
 
             BufferedReader br = null;
@@ -81,6 +38,7 @@ public class Main {
             instMem.setData(i++, tempInst);
         }
 */
+/*
         //check instruction memmory has got the data
         System.out.println(instMem.instructionMemory);
 
@@ -95,6 +53,11 @@ public class Main {
 
 */
 package com.foxploit;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
 
@@ -119,9 +82,38 @@ public class Main {
         RegisterFile regFile = new RegisterFile();
 
         int i = 0;
-        for (String tempInst: smpl_instr) {
-            InstructionMemory.setData(i++, tempInst);
+        BufferedReader br = null;
+        int count=0;
+        try {
+            br = new BufferedReader(new FileReader("input.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
+        String strLine = "";
+        while (strLine != null)
+        {
+            try {
+                strLine =br.readLine();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (strLine==null)
+                break;
+            //add to the hashmap
+            InstructionMemory.setData(count,strLine);
+
+
+            count++;
+        }
+        try {
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+//        for (String tempInst: smpl_instr) {
+//            InstructionMemory.setData(i++, tempInst);
+//        }
 
         CPU cpu = new CPU();
         cpu.process(regFile);
